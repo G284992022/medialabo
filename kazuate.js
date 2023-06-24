724316f38540c1df5a6970e97784720a449a7663
G284992022 <105624232+G284992022@users.noreply.github.com>
2023-06-24 20:01:57 +0900

    数当て

--- kazuate.js
+++ kazuate.js
@@ -17,23 +17,23 @@ function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
   kaisu = kaisu + 1;
-  console.log(kaisu + '回目の予想: ' + 4);
+  console.log(kaisu + '回目の予想: ' + yoso);
   if(kaisu>=4){
     console.log("答えは" + kotae + "でした.すでにゲームは終了しています.");
   }
-  if(kotae!==4){
+  if(kotae!==yoso){
     if(kaisu===3){
         console.log("まちがい.残念でした答えは" + kotae + "です.");
     }else if(kaisu>3){
         console.log("答えは" +  kotae +  "でした．すでにゲームは終わっています")
     }else{
-        if(kotae>4){
+        if(kotae>yoso){
             console.log("まちがい．答えはもっと大きいですよ");
-        }else if(kotae<4){
+        }else if(kotae<yoso){
             console.log("まちがい．答えはもっと小さいですよ");
         }
     }
-  }else if(kotae===4){
+  }else if(kotae===yoso){
     console.log("正解です．おめでとう!");
   }
   
