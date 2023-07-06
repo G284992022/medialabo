494e5c175a5e4b0610565783684e52c40068f989
G284992022 <105624232+G284992022@users.noreply.github.com>
2023-07-06 17:01:44 +0900

    数当て

--- kazuate.js
+++ kazuate.js
@@ -6,45 +6,44 @@ let kaisu = 0;
 let x1 = document.querySelector('p#result');
 let span = document.querySelector('span#kaisu');
 let answer = document.querySelector('span#answer');
+let b1 = document.querySelector('button#answer');
+b1.addEventListener('click', kaisuup);
+let yoso;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-/*hantei();
-hantei();
-hantei();*/
-
+function kaisuup(){
+  kaisu = kaisu + 1;
+  let i = document.querySelector('input[name="yoso"]');
+	yoso = Number(i.value);
+  hantei();
+}
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
-  kaisu = kaisu + 1;
-  console.log(kaisu + '回目の予想: ' + yoso);
+  let p = document.querySelector('p');
   span.textContent = kaisu;
   answer.textContent = yoso;
+  p.textContent=kaisu + '回目の予想:' + yoso;
   if(kaisu>=4){
-    console.log("答えは" + kotae + "でした.すでにゲームは終了しています.");
     x1.textContent="答えは" + kotae + "でした.すでにゲームは終了しています.";
   }
-  if(kotae!==yoso){
+  else if(kotae!==yoso){
     if(kaisu===3){
-        console.log("まちがい.残念でした答えは" + kotae + "です.");
         x1.textContent="まちがい.残念でした答えは" + kotae + "です.";
-    }else if(kaisu>3){
-        console.log("答えは" +  kotae +  "でした．すでにゲームは終わっています");
-        x1.textContent="答えは" +  kotae +  "でした．すでにゲームは終わっています";
-    }else{
+    }else {
         if(kotae>yoso){
-            console.log("まちがい．答えはもっと大きいですよ");
             x1.textContent="まちがい．答えはもっと大きいですよ";
         }else if(kotae<yoso){
-            console.log("まちがい．答えはもっと小さいですよ");
             x1.textContent="まちがい．答えはもっと少ないですよ";
         }
     }
   }else if(kotae===yoso){
-    console.log("正解です．おめでとう!");
-    x1.textContent="正解です．おめでとう!";
+    if(kaisu>3){
+        x1.textContent="答えは" +  kotae +  "でした．すでにゲームは終わっています";
+    }else{
+      x1.textContent="正解です．おめでとう!";
+    }
   }
   
   // 課題3-1: 正解判定する
